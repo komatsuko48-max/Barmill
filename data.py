6896a7dc76d5b82a25e555af62f7d9dd18512b5e
komatsuko48-max <komatsuko48@gmail.com>
2026-07-14 14:20:43 +0630

    Create data.py

---
+++ data.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+def load_offers():
+    try:
+        if not os.path.exists("data.json"):
+            print("data.json file not found")  # log အတွက်
+            return []
+        with open("data.json", "r", encoding="utf-8") as f:
+            data = json.load(f)
+            if not isinstance(data, list):
+                return []
+            return data
+    except Exception as e:
+        print(f"Error loading data.json: {e}")
+        return []
+
+offers = load_offers()
